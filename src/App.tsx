809f30d8cb01a92aa03b7dd389b9cb45302d439a
Manu Krishna <krishnamanu27112004@gmail.com>
2026-04-09 19:54:24 +0530

    feat: Added routes for the application and managed some protected routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 function App() {
     return (
         <>
+            <Routes>
+                <Route path="/" element={<>Hello</>} />
+                <Route path="/blogs" element={<ProtectedRoute><></></ProtectedRoute>} />
+                <Route path="/add" element={<ProtectedRoute><></></ProtectedRoute>} />
+                <Route path="/edit/:id" element={<ProtectedRoute><></></ProtectedRoute>} />
+
+            </Routes>
         </>
     );
 }
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,9 +3,9 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
