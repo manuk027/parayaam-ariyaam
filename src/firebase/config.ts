e8ea00f115cb931e6dceeb66adcf8e62db91e8a6
Manu Krishna <krishnamanu27112004@gmail.com>
2026-04-09 19:04:03 +0530

    feat: Setup firebase configuration for authentication and firestore.

---
+++ src/firebase/config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const useAuth = getAuth(app);
+export const db = getFirestore(app);
