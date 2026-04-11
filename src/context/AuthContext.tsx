44121484737129607e35b8b1e05ad21a5fc2e442
Manu Krishna <krishnamanu27112004@gmail.com>
2026-04-11 18:40:50 +0530

    fix: Separated the custome hook to different folder.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,19 +1,13 @@
-import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
 import { auth } from "../firebase/config";
 import { onAuthStateChanged, type User } from "firebase/auth";
 
-type AuthContextType = {
+export type AuthContextType = {
     user: User | null;
     loading: boolean;
 };
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error("useAuth must be used within AuthProvider");
-    return context;
-};
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 type AuthProviderProps = {
     children: ReactNode;
@@ -29,7 +23,6 @@ export default function AuthProvider({ children }: AuthProviderProps) {
         });
         return () => unsubscribe();
     }, []);
-
     return (
         <AuthContext.Provider value={{ user, loading }}>
             {!loading && children}
