1f2e8940f388a912ed4203dd56e8f03ecd52ba4a
Manu Krishna <krishnamanu27112004@gmail.com>
2026-04-17 14:06:58 +0530

    fix: Changed the UI for all stories and my stories and fixed the build issues in last commit.

--- src/pages/SingleBlog.tsx
+++ src/pages/SingleBlog.tsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Timestamp, doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
-import { Calendar, User, ArrowLeft, Clock } from "lucide-react";
+import { Calendar, User, ArrowLeft } from "lucide-react";
 
 export type Blog = {
     id: string;
